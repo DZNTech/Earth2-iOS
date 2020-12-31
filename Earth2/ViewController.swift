0261ed81fcb61bc5208418e38553a586d1bc6489
dzenbot <ignacio.romeroz@gmail.com>
2020-12-31 02:26:32 -0800

    Initial commit

---
+++ Earth2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Earth2
+//
+//  Created by Ignacio Romero Zurbuchen on 2020-12-31.
+//
+
+import UIKit
+import Kanna
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
