bf60bce17fca526cdf6ede0ec4ee1f5d41084478
dzenbot <ignacio.romeroz@gmail.com>
2021-01-08 15:52:56 -0800

    Kicking off the front end

---
+++ Earth2/RootViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RootViewController.swift
+//  Earth2
+//
+//  Created by Ignacio Romero Zurbuchen on 2021-01-08.
+//  Copyright © 2021 DZN Technologies Inc. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
