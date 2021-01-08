d0ce80d300fde894796c10d8e6dd39d4c5ca1462
dzenbot <ignacio.romeroz@gmail.com>
2021-01-08 03:13:43 -0800

    Installing pods + implemented User model

--- E2API/Models/User.swift
+++ E2API/Models/User.swift
@@ -6,19 +6,35 @@
 //
 
 import Foundation
+import ObjectMapper
 
-public class User {
+public class User: ImmutableMappable {
 
-//    public let id: ObjectId
-//    public let username: String
-//    public let country: String
-//    public let phone: String?
-//    public let referral_code: String
-//    public let imageUrl: String
-//    public let property_count: Int
-//    public let netWorth: Double
-//    public let balance: Double
-//    public let propertyIncreaseNet: Double
-//    public let propertyIncreasePct: Double
+    public let id: ObjectId
+    public let username: String
+    public let country: String
+    public let phone: String?
+    public let referralCode: String
+    public let imageUrl: String
+    public let propertyCount: Int
+    public let netWorth: Double
+    public let balance: Double
+    public let propertyIncreaseNet: Double
+    public let propertyIncreasePct: Double
 
+    // MARK: - Initialization
+
+    required public init(map: Map) throws {
+        id = try map.value("id")
+        username = try map.value("username")
+        country = try map.value("country")
+        phone = try? map.value("phone")
+        referralCode = try map.value("referral_code")
+        imageUrl = try map.value("image_url")
+        propertyCount = try map.value("property_count")
+        netWorth = try map.value("net_worth")
+        balance = try map.value("balance")
+        propertyIncreaseNet = try map.value("property_increase_net")
+        propertyIncreasePct = try map.value("property_increase_pct")
+    }
 }
