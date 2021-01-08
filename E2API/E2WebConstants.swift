500fe111ae65e7018ea83753f6231286210774eb
dzenbot <ignacio.romeroz@gmail.com>
2021-01-08 03:20:56 -0800

    Including basic web constants

---
+++ E2API/E2WebConstants.swift
@@ -0,0 +1,16 @@
+//
+//  E2WebConstants.swift
+//  E2API
+//
+//  Created by Ignacio Romero Zurbuchen on 2021-01-08.
+//
+
+import Foundation
+
+public enum MGPWebConstant: String {
+    case home = "https://earth2.io/"
+    case login = "https://app.earth2.io/login/auth0"
+    case signup = "https://earth2.eu.auth0.com/u/signup?state=g6Fo2SBlZmVnUHFuOWZHOFJueE9WY3JlcFlMVTQxN0Z1RVJZa6N0aWTZIGVOMmNhbTRYckFfWFJldWVXZlZ2cWtyaGxuSkEtUnk0o2NpZNkgVEJaN012RVNPdEZWZEo5dlkxNlFPUXhKdHhFUWwwRmI"
+
+    case apiBase = "https://www.earth2api.com/"
+}
