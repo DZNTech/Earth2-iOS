6752b2ddeaca9e25bb5c76c218b6f04cd8dddc8f
dzenbot <ignacio.romeroz@gmail.com>
2021-01-29 15:41:38 -0800

    Tweaking FAQ copy

--- E2API/WebConstants.swift
+++ E2API/WebConstants.swift
@@ -18,7 +18,7 @@ public enum WebConstant: String {
     case faq = "https://earth2.io/faq"
 
     case feebackPrefill = "https://docs.google.com/forms/d/e/1FAIpQLSdVo_t4EDcBMENDKeYb2cecOiUrvzRxrBUdHYoOUpWM6_OOaA/viewform"
-    case feebackShort = "forms.gle/nkn4YFjukjXy8DKz9"
+    case feebackShort = "https://forms.gle/nkn4YFjukjXy8DKz9"
 
     case testFlight = "testflight.apple.com/join/q24YtYT6"
     case sourceCode = "github.com/DZNTech/Earth2-iOS"
